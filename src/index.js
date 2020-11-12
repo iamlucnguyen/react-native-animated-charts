5006a91d930f9a01b1b7147236c9e8cac7aecda8
Luc Nguyen <iamnguyentanluc@gmail.com>
2020-11-12 14:13:31 +0700

    add gradient area chart

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export { default as ChartPathProvider } from './charts/linear/ChartPathProvider'
 export { default as ChartDot } from './charts/linear/ChartDot';
 export { ChartYLabel, ChartXLabel } from './charts/linear/ChartLabels';
 export { default as ChartPath } from './charts/linear/ChartPath';
+export { default as ChartPathGradient } from './charts/linear/ChartPathGradient';
 export { default as useChartData } from './helpers/useChartData';
 export { default as simplifyData } from './simplification/simplifyData';
 export { default as monotoneCubicInterpolation } from './interpolations/monotoneCubicInterpolation';
