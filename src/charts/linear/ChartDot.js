3c11c7081b8dd60a72c63f9cd3d4d2eaf862194d
Lực Nguyễn <iamnguyentanluc@gmail.com>
2021-04-14 15:31:22 +0700

    add chart dot indicator

--- src/charts/linear/ChartDot.js
+++ src/charts/linear/ChartDot.js
@@ -2,28 +2,55 @@ import React, { useContext } from 'react';
 import Animated from 'react-native-reanimated';
 import ChartContext from '../../helpers/ChartContext';
 import withReanimatedFallback from '../../helpers/withReanimatedFallback';
+import LinearGradient from 'react-native-linear-gradient';
 
-function ChartDot({ style, size = 10, ...props }) {
-  const { dotStyle } = useContext(ChartContext);
+function ChartDot({ style, size = 10, indicator = false, indicatorColor = ["#000", "#000"], ...props }) {
+	const { dotStyle, indicatorStyle, positionX, positionY } = useContext(ChartContext);
 
-  return (
-    <Animated.View
-      {...props}
-      pointerEvents="none"
-      style={[
-        dotStyle,
-        {
-          borderRadius: size / 2,
-          height: size,
-          left: -size / 2,
-          position: 'absolute',
-          top: -size / 2,
-          width: size,
-        },
-        style,
-      ]}
-    />
-  );
+	const renderIndicator = () => {
+		if (indicator) {
+			return (
+				<Animated.View
+					style={[
+						indicatorStyle,
+						{
+							width: 1.5,
+							transform: [
+								{ translateX: positionX.value + 4 },
+								{ translateY: positionY.value + 2 }
+							]
+						}]}>
+					<LinearGradient
+						colors={indicatorColor}
+						style={{
+							height: "95%", width: "100%"
+						}}
+					/>
+				</Animated.View>
+			)
+		} else return null;
+	}
+
+	return (
+		<Animated.View
+			{...props}
+			pointerEvents="none"
+			style={[
+				dotStyle,
+				{
+					borderRadius: size / 2,
+					height: size,
+					left: -size / 2,
+					position: 'absolute',
+					top: -size / 2,
+					width: size
+				},
+				style,
+			]}
+		>
+			{renderIndicator()}
+		</Animated.View >
+	);
 }
 
 export default withReanimatedFallback(ChartDot);
--- src/charts/linear/ChartPathProvider.js
+++ src/charts/linear/ChartPathProvider.js
@@ -2,34 +2,42 @@ import React, { useMemo, useState } from 'react';
 import { useAnimatedStyle } from 'react-native-reanimated';
 import ChartContext, { useGenerateValues } from '../../helpers/ChartContext';
 
-export default function ChartPathProvider({ data: providedData, children }) {
-  const values = useGenerateValues();
-  const dotStyle = useAnimatedStyle(
-    () => ({
-      opacity: values.dotScale.value,
-      transform: [
-        { translateX: values.positionX.value },
-        { translateY: values.positionY.value + 10 }, // TODO temporary fix for clipped chart
-        { scale: values.dotScale.value },
-      ],
-    }),
-    []
-  );
-  const [contextReanimatedValue, setContextValue] = useState({});
-  const contextValue = useMemo(
-    () => ({
-      dotStyle,
-      ...values,
-      ...contextReanimatedValue,
-      providedData,
-      setContextValue,
-    }),
-    [dotStyle, values, contextReanimatedValue, providedData]
-  );
+export default function ChartPathProvider({ data: providedData, size, children }) {
+	const values = useGenerateValues();
+	const dotStyle = useAnimatedStyle(
+		() => ({
+			opacity: values.dotScale.value,
+			transform: [
+				{ translateX: values.positionX.value },
+				{ translateY: values.positionY.value + 10 }, // TODO temporary fix for clipped chart
+				{ scale: values.dotScale.value },
+			],
+		}),
+		[]
+	);
+	const indicatorStyle = useAnimatedStyle(
+		() => ({
+			height: size - values.positionY.value
+		}),
+		[]
+	);
 
-  return (
-    <ChartContext.Provider value={contextValue}>
-      {children}
-    </ChartContext.Provider>
-  );
+	const [contextReanimatedValue, setContextValue] = useState({});
+	const contextValue = useMemo(
+		() => ({
+			dotStyle,
+			indicatorStyle,
+			...values,
+			...contextReanimatedValue,
+			providedData,
+			setContextValue,
+		}),
+		[dotStyle, indicatorStyle, values, contextReanimatedValue, providedData]
+	);
+
+	return (
+		<ChartContext.Provider value={contextValue}>
+			{children}
+		</ChartContext.Provider>
+	);
 }
